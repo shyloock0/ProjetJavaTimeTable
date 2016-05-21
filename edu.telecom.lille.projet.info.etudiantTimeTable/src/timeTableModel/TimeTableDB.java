4eb6d69b57e2d91d2dd2a438ce9b9421198400cc
sarah achour thiebault <sachour@elv.telecom-lille.fr>
2016-05-21 02:08:59 +0200

    add et remove de toutes les fonctions save() et load() probleme avec les dates

--- edu.telecom.lille.projet.info.etudiantTimeTable/src/timeTableModel/TimeTableDB.java
+++ edu.telecom.lille.projet.info.etudiantTimeTable/src/timeTableModel/TimeTableDB.java
@@ -1,13 +1,12 @@
 package timeTableModel;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.input.SAXBuilder;
-import org.jdom2.output.Format;
-import org.jdom2.output.XMLOutputter;
+import java.io.*;
+import org.jdom2.*;
+import org.jdom2.output.*;
+import org.jdom2.input.*;
+import java.util.List;
+import java.util.Date;
+import java.util.Iterator;
 
 /**
  * 
@@ -28,8 +27,11 @@ public class TimeTableDB {
 	 * Le fichier contenant la base de données.
 	 * 
 	 */
-	Element rootElt1=new Element("Rooms");
-	Element rootElt2=new Element("TimeTables");
+	//création du fichier doc (Document)
+	Element rootElt=new Element("TimeTableDB");
+	org.jdom2.Document doc = new Document(rootElt);
+
+	
 	private String file;
 	/**
 	 * 
@@ -62,68 +64,208 @@ public class TimeTableDB {
 		this.file = file;
 	}
 		
-	
+	//sauvergarde
 	public void saveDB(){
 		try{
 			XMLOutputter sortie =new XMLOutputter(Format.getPrettyFormat());
-			sortie.output(sortie,setFile("file"));
-
-		}
-		catch (IOException e){}
+			sortie.output(doc,new FileOutputStream(file));
+		}catch (IOException e){}
 	}
 	
 	
 	public void loadDB(){
-		org.jdom2.Document document = null;//création d'un document vide
-		Element rootElt1;
-		Element rootElt2;
-		SAXBuilder sxb =new SAXBuilder();
 		try{
-			document = sxb.build(InputSource("timeTableDB"));
-			
+			SAXBuilder sxb = new SAXBuilder();
+			doc = sxb.build(new File(file));
+			rootElt= doc.getRootElement();
+			}catch (IOException | JDOMException e){}//bizarre
 		}
-		catch(Exception e){}	
+	
+	public void removeRoom(int roomId){
+		//charge la base de donnee
+		loadDB();
+		
+		Element roomsElt= rootElt.getChild("Rooms");
+	    List<Element> ListroomElt = roomsElt.getChildren("Room");
+	    Iterator<Element> i = ListroomElt.iterator();
+	    //On parcours la liste grâce à un iterator
+	    
+	    while(i.hasNext()){
+	    	Element courant = (Element)i.next();
+	        //Si l’étudiant possède l'Element en question on applique
+	        //les modifications.
+	    	
+	    	if(courant.getChild("RoomId").getText()==Integer.toString(roomId)){
+	          //On supprime l'Element en question en passant par sont parrent
+	          roomsElt.removeContent(courant);
+	    	}
+	    }
+	    
+	    //enregistre les modif dans la base de donnees
+	    saveDB();
 	}
 	
 
 	public void addRoom(int roomId, int capacity){
-			String room = Integer.toString (roomId);
-			Room room= new Room(roomId,capacity);
-			
-			
-			//					JDOM
-			//creation d'une sous classe Room de Rooms
-			Element RoomElt1 = new Element("Room");
-			RoomElt1.addContent(rootElt1);
-			//creation d'une sous classe RoomId de Room avec pour contenue roomId
-			Element IdRoomElt1 = new Element("RoomId");
-			IdRoomElt1.setText(Integer.toString(roomId));
-			RoomElt1.addContent(IdRoomElt1);
-			//creation d'une sous classe Capacity de Room avec pour contenue capacity
-			Element  CapacityRoomElt1 = new Element("Capacity");
-			CapacityRoomElt1.setText(Integer.toString(capacity));
-			RoomElt1.addContent(CapacityRoomElt1);
-			saveDB();
-			
-	}
-	
-	
-	public void removeRoom(int roomId){
+		
 		loadDB();
 		
-		
-		
+		//melange de deux techniques
+		Element RoomsElt= rootElt.getChild("Rooms");
+			
+		//					JDOM
+		//creation d'une sous classe Room de Rooms
+		Element RoomElt = new Element("Room");
+		RoomsElt.addContent(RoomElt);
+		//creation d'une sous classe RoomId de Room avec pour contenue roomId
+		Element IdRoomElt = new Element("RoomId");
+		IdRoomElt.setText(Integer.toString(roomId));
+		RoomElt.addContent(IdRoomElt);
+		//creation d'une sous classe Capacity de Room avec pour contenue capacity
+		Element  CapacityRoomElt = new Element("Capacity");
+		CapacityRoomElt.setText(Integer.toString(capacity));
+		RoomElt.addContent(CapacityRoomElt);
+		saveDB();
+			
 	}
+	
 
 	
 	public void addTimeTable(int timeTableId){
-		String timeTable = Integer.toString(timeTableId);
-		TimeTable timeTable= new TimeTable(timeTableId);
-		///JDOM
+		loadDB();
 		
+		//melange de deux techniques
+		Element TimeTablesElt= rootElt.getChild("TimeTables");
+			
+		Element TimeTableElt = new Element("TimeTable");
+		TimeTablesElt.addContent(TimeTableElt);
+		
+		//creation de la sous classe GroupID
+		Element GroupIdElt = new Element("GroupId");
+		GroupIdElt.setText(Integer.toString(timeTableId));
+		TimeTableElt.addContent(GroupIdElt);
+		
+		//creation de la sous classe Books
+		Element BooksElt = new Element("Books");
+		TimeTableElt.addContent(BooksElt);
+
+		saveDB();	
+	}
+	
+	
+	
+	public void removeTimeTable(int timeTableId){
+		//meme principe que pour removeRoom()
+		
+		//charge la base de donnee
+		loadDB();
+				
+		Element TimeTablesElt= rootElt.getChild("TimeTables");
+		List<Element> ListTimeTableElt = TimeTablesElt.getChildren("TimeTable");
+		Iterator<Element> i = ListTimeTableElt.iterator();
+		//On parcours la liste grâce à un iterator
+		while(i.hasNext()){
+			Element courant = (Element)i.next();
+			//Si l’étudiant possède l'Element en question on applique les modifications.
+			if(courant.getChild("timeTableId").getText()==Integer.toString(timeTableId)){
+				//On supprime l'Element en question en passant par sont parrent
+				TimeTablesElt.removeContent(courant);
+				}
+			}
+			    
+		//enregistre les modif dans la base de donnees
 		saveDB();
+		}
+	
+	public void removeBook(int timeTableId, int bookId){
+		//meme principe que pour removeRoom()
+		
+				//charge la base de donnee
+				loadDB();
+						
+				Element TimeTablesElt= rootElt.getChild("TimeTables");
+				List<Element> ListTimeTableElt = TimeTablesElt.getChildren("TimeTable");
+				Iterator<Element> i = ListTimeTableElt.iterator();
+				//On parcours la liste grâce à un iterator
+				while(i.hasNext()){
+					Element courant = (Element)i.next();
+					//Si on est dans le bonne emploi du temps
+					if(courant.getChild("timeTableId").getText()==Integer.toString(timeTableId)){
+						//on reitere le processus
+						
+						//on creer la liste des Elt ded reservation de courant
+						List<Element> ListBookElt = courant.getChildren("Book");
+						Iterator<Element> k = ListBookElt.iterator();
+						
+						while(k.hasNext()){
+							Element courantbis = (Element)k.next();
+							
+							if(courantbis.getChild("bookId").getText()==Integer.toString(bookId)){
+								courant.removeContent(courantbis);
+							}
+						}
+					}
+					    
+				//enregistre les modif dans la base de donnees
+				saveDB();
+				}
 		
 	}
+
+	public void addBook(int timeTableId, int bookId, String login, Date dateBegin, Date dateEnd, int roomId){
+		loadDB();
+		
+		//melange de deux techniques
+		Element TimeTablesElt= rootElt.getChild("TimeTables");
+		
+		//on recupere le bonne emploi du temps
+		List<Element> ListTimeTableElt = TimeTablesElt.getChildren("TimeTable");
+		Iterator<Element> i = ListTimeTableElt.iterator();
+
+		while(i.hasNext()){
+			Element courant = (Element)i.next();
+			if(courant.getChild("timeTableId").getText()==Integer.toString(timeTableId)){
+				//creation d'une classe book
+				Element BookElt = new Element("Book");
+				courant.addContent(BookElt);
+				//ajout de BookId
+				Element BookIdElt = new Element("BookId");
+				BookIdElt.setText(Integer.toString(bookId));
+				BookElt.addContent(BookIdElt);
+				//ajout de login
+				Element LoginElt = new Element("Login");
+				LoginElt.setText(login);
+				BookElt.addContent(LoginElt);
+				//ajout de DateBegin
+				Element DateBeginElt = new Element("DateBegin");
+					//problem conversion de date :DateBeginElt.setText(Date.toString(dateBegin)));
+				BookElt.addContent(DateBeginElt);
+				//ajout de DateBegin
+				Element DateEndElt = new Element("DateEnd");
+					//problem conversion de date :DateEndElt.setText(Date.toString(dateEnd)));
+				BookElt.addContent(DateEndElt);
+				//ajout de roomId
+				Element RoomIdElt = new Element("RoomId");
+				BookIdElt.setText(Integer.toString(roomId));
+				BookElt.addContent(RoomIdElt);	
+			}
+
+		saveDB();
+		}
+	}	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
